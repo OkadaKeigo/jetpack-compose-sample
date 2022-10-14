a5d6f9502f1b7404abc627aa0d27a5a4e5a57483
OkadaKeigo <k5ok4d4@gmail.com>
2022-10-14 10:08:16 +0900

    プロジェクト作成

---
+++ app/src/main/java/com/gmail/k5ok4d4/jetpackcomposesample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.k5ok4d4.jetpackcomposesample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

6755ce9da08acc913ebf99150222befbb4938c88
OkadaKeigo <k5ok4d4@gmail.com>
2022-10-14 12:03:26 +0900

    add: テキスト表示 Hello Android!

--- app/src/main/java/com/gmail/k5ok4d4/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/gmail/k5ok4d4/jetpackcomposesample/MainActivity.kt
@@ -2,10 +2,20 @@ package com.gmail.k5ok4d4.jetpackcomposesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MessageCard("Android")
+        }
     }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
 }
